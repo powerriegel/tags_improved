82cb9e5dc30d9dce325f0a87f6acc3f70d963413
Powerriegel <powerriegel@posteo.de>
2021-11-01 17:37:19 +0100

    First improvements

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 
-//Based on original Bludit tags plugin 
+//Based on original Bludit tags plugin
 //author: Mirivlad
 //version: 0.0.1
 
@@ -30,6 +30,11 @@ class pluginTagsPlus extends Plugin {
 		return $html;
 	}
 
+	public function siteHead()
+	{
+		return $this->includeCSS('tagsimproved.css');
+	}
+
 	public function siteSidebar()
 	{
 		global $L;
@@ -39,37 +44,30 @@ class pluginTagsPlus extends Plugin {
 		$filter = $url->filters('tag');
 
 		$html  = '<div class="plugin plugin-tags">';
-		$html .= '<h2 class="plugin-label">'.$this->getValue('label').'</h2>';
+		$html .= '<h2 class="plugin-label">'.htmlentities($this->getValue('label')).'</h2>';
 		$html .= '<div class="plugin-content">';
 		$html .= '<ul class="tags">';
 
-		// By default the database of tags are alphanumeric sorted
-		foreach( $tags->db as $key=>$fields ) {
-		    $fontSize = "";
-		    $fontWeight = "400";
-		    if (count($fields['list']) <= 1) {
-                $size = "";
-            } else if(count($fields['list']) > 1 && count($fields['list']) <= 4) {
-				$fontSize = "large";
-				$fontWeight = "600";
-			} else if(count($fields['list']) > 4 && count($fields['list']) <= 8) {
-				$fontSize = "larger";
-				$fontWeight = "700";
-			} else if(count($fields['list'])> 8 && count($fields['list']) <= 13) {
-				$fontSize = "x-large";
-				$fontWeight = "700";
+		foreach( $tags->db as $ke y=> $fields ) {
+
+			$current = count$fields['list']);
+			$class = 'xsmall';
+
+		    if ($current < 1) ) {
+				$class = 'xsmall';
+            } else if($current) > 1 && count($current) <= 3) {
+				$small = 'small';
+			} else if(count($current) > 3 && $current <= 6) {
+				$class = 'medium';
+			} else if($current > 6 && count($fields['list']) <= 12) {
+				$class = 'large';
 			} else {
-                $fontSize = "xx-large";
-				$fontWeight = "700";
+				$class = "xlarge";
 			}
 
-            if($fontSize == "") {
-                $html .= '<li>';
-            } else {
-                $html .= '<li style="font-size: ' . $fontSize . '; font-weight: ' . $fontWeight . '">';
-            }
-            $html .= '<a href="'.DOMAIN_TAGS.$key.'">';
-            $html .= $fields['name'];
+            $html .= '<li class="'.htmlentities($class).'">';
+            $html .= '<a href="'.DOMAIN_TAGS.htmlentities($key).'">';
+            $html .= htmlentities($fields['name']);
             $html .= '</a>';
             $html .= '</li>';
 		}
